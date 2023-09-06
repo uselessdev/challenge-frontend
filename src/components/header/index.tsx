e405755998be257a7bcbf668ffbb0dccedfac90c
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2023-09-06 20:27:38 -0300

    refactor(header): responsive header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,25 +1,24 @@
 import React, { useState } from 'react'
-import { Container, Content, ContentSearch, ContentUser } from './styles'
+import { Container, Content, ContentSearch, ContentUser, Logo } from './styles'
+
 import Search from '../search'
 
 const Header: React.FC = () => {
   const [term, setTerm] = useState('')
+
   return (
     <Container>
       <Content>
         <div>
           <a href="/">
-            <img
-              src="https://s3-sa-east-1.amazonaws.com/files.arvoredelivros.com.br/arvore-library-assets/images/logos/logo-livros-horizontal-white.svg"
-              alt="Arvore Livros"
-              width="168"
-              height="25"
-            />
+            <Logo />
           </a>
         </div>
+
         <ContentSearch>
           <Search value={term} />
         </ContentSearch>
+
         <ContentUser>
           <p>login</p>
         </ContentUser>
--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -1,50 +1,43 @@
 import styled from 'styled-components'
+import { ReactComponent as WhiteLogo } from '../../assets/logo.svg'
 
-export const Container = styled.div`
-  background-color: #45d0c1;
-  position: absolute;
-  top: 0;
-  padding: 16px;
+export const Container = styled.header`
   width: 100%;
-
-  @media (min-width: 992px) {
-    left: 0;
-    right: 0;
-  }
+  padding: ${props => props.theme.sizes[4]};
+  background-color: ${props => props.theme.colors.green};
 `
 
-export const Content = styled.div`
+export const Content = styled.section`
+  width: 100%;
+  max-width: ${props => props.theme.sizes.container};
   display: flex;
+  gap: ${props => props.theme.sizes[4]};
   flex-direction: column;
   align-items: center;
 
-  @media (min-width: 992px) {
+  @media (min-width: ${props => props.theme.screens.desktop}) {
     margin: 0 auto;
     flex-direction: row;
     justify-content: space-between;
   }
-  @media (min-width: 1440px) {
-    max-width: 1433px;
-  }
 `
 
 export const ContentSearch = styled.div`
-  display: flex;
-  align-items: center;
-  width: 96%;
-  margin: 16px 0 8px;
-  padding-right: 14px;
+  width: 100%;
 
-  @media (min-width: 992px) {
-    width: 70%;
-    margin: 0;
-    padding-right: 0;
+  @media (min-width: ${props => props.theme.screens.desktop}) {
+    max-width: 70%;
   }
 `
 
 export const ContentUser = styled.div`
   display: none;
-  @media (min-width: 992px) {
+
+  @media (min-width: ${props => props.theme.screens.desktop}) {
     display: block;
   }
+`
+
+export const Logo = styled(WhiteLogo)`
+  height: 25px;
 `
