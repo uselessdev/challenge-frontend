415cc38e46c2ba2f97c964a85b6fee3a8fcb91c4
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2023-09-12 15:10:23 -0300

    chore(msw): enable mock only in test mode

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { AppWithProviders } from './providers.tsx'
 
-// if (!import.meta.env.PROD) {
-//   const { worker } = await import('./mocks/browser.ts');
-//   worker.start();
-// }
+if (import.meta.env.MODE === 'test') {
+  const { worker } = await import('./mocks/browser.ts');
+  worker.start();
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
